8d645bd8100f742908db4321776b2fecd5b6d7f5
houyuao <houyuao@vipkid.com.cn>
2021-02-08 11:23:57 +0800

    添加user

---
+++ src/main/java/com/study/entity/User.java
@@ -0,0 +1,39 @@
+package com.study.entity;
+
+/**
+ * @author houyutao
+ * @date 2021-02-08
+ */
+
+public class User {
+
+    private Integer age;
+
+    private Long id;
+
+    private String name;
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
